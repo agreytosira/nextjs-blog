364db0571bbce1a36df5e91ac43cf52369d48ff5
Agrey Tosira <agreytosira@gmail.com>
2023-12-25 02:57:10 +0700

    init: setup starter components

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Pagination } from '../pagination/Pagination';
+import styles from './cardList.module.css';
+
+export const CardList = () => {
+    return (
+        <div className='{styles.container}'>
+            CardList
+            <Pagination />
+        </div>
+    );
+};
