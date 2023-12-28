55efdc9908e83166f9e8c07c5d4b2be5435b64eb
Agrey Tosira <agreytosira@gmail.com>
2023-12-28 20:45:04 +0700

    feat: finishing editors pick section

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -10,6 +10,72 @@ export const Menu = () => {
             <h1 className={styles.title}>Most Popular</h1>
             <div className={styles.items}>
                 <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.style}`}>style</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.fashion}`}>fashion</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.food}`}>food</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.culture}`}>culture</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/' className={styles.item}>
+                    <div className={styles.textContainer}>
+                        <span className={`${styles.category} ${styles.coding}`}>coding</span>
+                        <h3 className={styles.postTitle}>Lorem, ipsum dolor sit amet </h3>
+                        <div className={styles.detail}>
+                            <span className={styles.username}>John Doe</span>
+                            <span className={styles.date}> - 10.03.2023</span>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+            {/* Editor Pick Section */}
+            <h2 className={styles.subtitle}>Chosen by the editor</h2>
+            <h1 className={styles.title}>Editors Pick</h1>
+            <div className={styles.items}>
+                <Link href='/' className={styles.item}>
                     <div className={styles.imageContainer}>
                         <Image src='/p1.jpeg' alt='' fill className={styles.image} />
                     </div>
