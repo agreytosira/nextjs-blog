11b89e10bfcfcd20181dd8f6ffdd0129e8bbbc6e
Agrey Tosira <agreytosira@gmail.com>
2024-01-01 21:41:25 +0700

    feat: finishing menu categories section

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import MenuCategories from '../menuCategories/MenuCategories';
 import { MenuPosts } from '../menuPosts/menuPosts';
 import styles from './menu.module.css';
 
@@ -14,26 +15,7 @@ export const Menu = () => {
             {/* Categories Section */}
             <h2 className={styles.subtitle}>Discover by topic</h2>
             <h1 className={styles.title}>Categories</h1>
-            <div className={styles.categoryList}>
-                <Link href='/blog?cat=style' className={`${styles.categoryItem} ${styles.style}`}>
-                    style
-                </Link>
-                <Link href='/blog?cat=fashion' className={`${styles.categoryItem} ${styles.fashion}`}>
-                    fashion
-                </Link>
-                <Link href='/blog?cat=food' className={`${styles.categoryItem} ${styles.food}`}>
-                    food
-                </Link>
-                <Link href='/blog?cat=travel' className={`${styles.categoryItem} ${styles.travel}`}>
-                    travel
-                </Link>
-                <Link href='/blog?cat=culture' className={`${styles.categoryItem} ${styles.culture}`}>
-                    culture
-                </Link>
-                <Link href='/blog?cat=coding' className={`${styles.categoryItem} ${styles.coding}`}>
-                    coding
-                </Link>
-            </div>
+            <MenuCategories />
 
             {/* Editor Pick Section */}
             <h2 className={styles.subtitle}>Chosen by the editor</h2>
